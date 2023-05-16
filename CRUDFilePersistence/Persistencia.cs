bfb215fa4a805a1fdc03e4c706e4a2edb13eb6e6
Taylor Oliveira <taylorso2004@hotmail.com>
2023-05-16 15:25:31 -0300

    Initial commit

---
+++ CRUDFilePersistence/Persistencia.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDFilePersistence
+{
+    internal class Persistencia
+    {
+        public static void lerArquivoParaTela(string nomeArquivo)
+        {
+            try
+            {
+                StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8);
+                do
+                {
+                    Console.WriteLine(leitor.ReadToEnd());
+                } while (leitor.EndOfStream);
+
+                leitor.Close();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Problemas com o arquivo!");
+            }
+
+        }
+
+        public static void lerArquivoExibeNomes(string nomeArquivo)
+        {
+            StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8);
+            string[] vetorLinha;
+            string linha;
+            do
+            {
+                linha = leitor.ReadLine();
+                vetorLinha = linha.Split(";");
+                Console.WriteLine(vetorLinha[0]);
+            } while (!leitor.EndOfStream);
+            leitor.Close();
+        }
+
+        public static void popularArquivoLista(string nomeArquivo, List<Pessoa> lista)
+        {
+            StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8);
+
+            string[] vetorLinha;
+            string linha;
+
+            do
+            {
+                linha = leitor.ReadLine();
+                vetorLinha = linha.Split(";");
+
+                lista.Add(new Pessoa(vetorLinha[0], vetorLinha[1]));
+
+            } while (!leitor.EndOfStream);
+            leitor.Close();
+        }
+
+        public static void gravarListaArquivo(List<Pessoa> lista, string nomeArquivo)
+        {
+            StreamWriter escritor = new StreamWriter(nomeArquivo, append: true);
+
+            foreach (var item in lista)
+            {
+                escritor.WriteLine($"{item.Nome};{item.Email};{item.DataNascimento}");
+                escritor.Flush();
+            }
+
+            escritor.Close();
+        }
+
+        public static void atualizarPessoaArquivo(Pessoa pessoa, string nomeArquivo)
+        {
+            StreamWriter escritor = new StreamWriter(nomeArquivo, append: true);
+
+            escritor.WriteLine($"{pessoa.Nome};{pessoa.Email};{pessoa.DataNascimento}");
+
+            escritor.Close();
+        }
+
+        public static void exibirLista(List<Pessoa> lista)
+        {
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item.Nome);
+            }
+        }
+    }
+}
---
+++ CRUDFilePersistence/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace CRUDFilePersistence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> list = new List<Pessoa>(); 
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("UFN - UNIVERSIDADE FRANSCISCANA");
+            Console.ResetColor();
+            Console.WriteLine("Seja muito bem vindo(a)!");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Escolha uma das opções para continuar");
+            Console.WriteLine(" ");
+
+            int opcao;
+            bool continuar = true;
+
+            while (continuar)
+            {
+                Console.WriteLine("1- Cadastrar pessoa");
+                Console.WriteLine("2- Listar pessoa");
+                Console.WriteLine("3- Apagar pessoa");
+                Console.WriteLine("4- Pesquisar pessoa");
+                Console.WriteLine("5- Sair");
+
+                Console.WriteLine(" ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Cadastrar");
+                    break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Listar"); 
+                    break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Apagar");
+                    break;
+
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Pesquisar");
+                    break;
+
+                    case 5:
+                        Console.Clear();
+                        Console.WriteLine("Saindo");
+                        continuar = false;
+                    break;
+
+                    default: 
+                        Console.WriteLine(" ");
+                        Console.WriteLine("Comandos inválidos, digite outra opção!");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+//Menu:
+
+//1 - Cadastrar pessoa
+//2- Listar Pessoa
+//3- Apagar pessoa
+
+//* Elementos necessários:
+
+//	1 - Classe Pessoa(nome, email, dataNascimento(string))
+//        - método para gerar o email da pessoa automaticamente:
+//			"sobrenome@ufn.edu.br"
+
+
+//    2 - Lista de Pessoas
+//		- Ordenação (Sort)
+
+//	3 - Arquivo
+//        - Texto puro->CSV ou seja, separada por ";"
+//		- StremReader (Classe leitora)
+// 		-StreamWriter(Classe escritora)
+
+//* Fluxo do Programa
+
+
+//    Cadastrar->Nome e DataNascimento -> Constrói Objeto com email gerado -> Joga na lista 
+//	-> Atualiza o arquivo
+
+//	Listar -> Mostrar na lista
+
+//	Pesquisar -> Percorrer a lista a procura de alguma pessoa
+
+//	Excluir -> Pesquisar -> Retirar da lista -> Atualizar arquivo
